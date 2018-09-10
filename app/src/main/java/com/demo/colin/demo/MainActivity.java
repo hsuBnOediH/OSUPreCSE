47c1f547ddcffe6361ff27c1bcb9b1e3ec9e2832
name <you@example.com>
2018-09-10 15:54:37 -0400

    0.0.1

---
+++ app/src/main/java/com/demo/colin/demo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.demo.colin.demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button preMajor;
+    private Button major;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        preMajor = findViewById(R.id.preMajor);
+        preMajor.setOnClickListener(preMajorListener);
+        major = findViewById(R.id.major);
+        major.setOnClickListener(majorListener);
+
+
+    }
+    Button.OnClickListener preMajorListener = new Button.OnClickListener() {
+        public void onClick(View v) {
+            Intent jumpPre = new Intent(com.demo.colin.demo.MainActivity.this, PreCSEActivity.class);
+            startActivity(jumpPre);
+            MainActivity.this.finish();
+        }
+    };
+
+    Button.OnClickListener majorListener = new Button.OnClickListener(){
+      public void onClick(View view){
+          Intent jumpMajor = new Intent(com.demo.colin.demo.MainActivity.this,MajorActivity.class);
+          startActivity(jumpMajor);
+      }
+    };
+}
