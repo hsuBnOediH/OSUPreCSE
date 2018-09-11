5de368e270369105efb729b612a4acfe131b5770
bigEyeChink <li.8906@buckeyemail.osu.edu>
2018-09-11 15:25:37 -0300

    add listview precse ap activity

---
+++ app/src/main/java/com/demo/colin/demo/APStandard.java
@@ -0,0 +1,13 @@
+package com.demo.colin.demo;
+
+public class APStandard {
+    private final int type;
+
+    public APStandard(int type){
+        this.type = type;
+    }
+
+    public int getType(){
+        return type;
+    }
+}
--- app/src/main/java/com/demo/colin/demo/PreCSEActivity.java
+++ app/src/main/java/com/demo/colin/demo/PreCSEActivity.java
@@ -2,12 +2,35 @@ package com.demo.colin.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PreCSEActivity extends AppCompatActivity {
+
+
+    ListView ap_list;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_cse);
+
+        ap_list = findViewById(R.id.ap_list);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.addAll(Arrays.asList(getResources().getStringArray(R.array.ap_list)));
+
+        adapter = new ArrayAdapter<>(
+                PreCSEActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrayList
+        );
+
+        ap_list.setAdapter(adapter);
+
     }
 }
