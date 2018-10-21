a278f7ba48fa965d21677f1b2cc185b184dd624e
name <you@example.com>
2018-10-21 17:42:13 -0400

    comment added -2

--- app/src/main/java/com/demo/colin/demo/PreCSEActivity.java
+++ app/src/main/java/com/demo/colin/demo/PreCSEActivity.java
@@ -15,18 +15,27 @@ public class PreCSEActivity extends AppCompatActivity {
 
     // 创建adapter
     private CheckBoxListAdapter checkBoxListAdapter;
+    // 创建选项的arrayList
     private ArrayList<String> contextList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_cse);
-        ListView listView = findViewById(R.id.lv);
+
+        // 找到List View
+        ListView listView = findViewById(R.id.ap_checkbox_list);
+        //选项 array List
         contextList = new ArrayList<>();
+        // 选项插入
         initData();
+        // 记录的选项选中的Map
         HashMap<Integer, Boolean> satisfyClass = new HashMap<>();
+        // 实体化Adapter
         checkBoxListAdapter = new CheckBoxListAdapter(contextList,this, satisfyClass);
+        // 关联ListView 和adapter
         listView.setAdapter(checkBoxListAdapter);
+        // 选项被选中的反应函数
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -36,11 +45,13 @@ public class PreCSEActivity extends AppCompatActivity {
                 dataChanged();
             }
         });
+        // 下一步的按钮设置 和响应函数
         Button nextStep = findViewById(R.id.ap_confirm_button);
         nextStep.setOnClickListener(nextStepListener);
     }
 
     private void initData(){
+        // 加入ListView 的选项
         contextList.add("我的ap可以换掉CSE1223");
         contextList.add("我的ap可以换掉MATH1151");
         contextList.add("我的ap可以换掉MATH1152");
@@ -49,31 +60,42 @@ public class PreCSEActivity extends AppCompatActivity {
         contextList.add("我的ap可以换掉PHY1251");
     }
 
+    // 选项变化后 更新展示的内容
     private void dataChanged(){
         checkBoxListAdapter.notifyDataSetChanged();
     }
 
+    // 下一步按键的响应函数
     Button.OnClickListener nextStepListener= new Button.OnClickListener() {
         public void onClick(View v) {
-            Intent jumpPre = new Intent(com.demo.colin.demo.PreCSEActivity.this, MajorActivity.class);
-            HashMap<Integer,Boolean> map = checkBoxListAdapter.getSelectedMap();
-            HashSet<String> set = new HashSet<>();
-
-            if(map.get(0)) set.add("CSE1223");
-
-            if(map.get(1))set.add("MATH1151");
-
-            if(map.get(2))set.add("MATH1152");
-
-            if(map.get(3)) set.add("ENG1110");
-
-            if(map.get(4)) set.add("PHY1250");
-
-            if(map.get(5)) set.add("PHY1251");
-
-            jumpPre.putExtra("Set",set);
+            //跳转下一个的页面
+            Intent jumpPre = new Intent(com.demo.colin.demo.PreCSEActivity.this, EngLishMath.class);
+            // pass set 到下一个页面
+            jumpPre.putExtra("Set",getSatifySet(checkBoxListAdapter.getSelectedMap()));
+            //jump 页面
             startActivity(jumpPre);
+            // 关闭当前页面
             PreCSEActivity.this.finish();
         }
     };
+
+    private HashSet<String> getSatisfySet(HashMap<Integer,Boolean> map) {
+        HashSet<String> set = new HashSet<>();
+        if(map.get(0)) set.add("CSE1223");
+
+        if(map.get(1))set.add("MATH1151");
+
+        if(map.get(2))set.add("MATH1152");
+
+        if(map.get(3)) set.add("ENG1110");
+
+        if(map.get(4)) set.add("PHY1250");
+
+        if(map.get(5)) set.add("PHY1251");
+
+        return set;
+    }
+
+
+
 }
