979a1497b6b0579f93a82a9fa87bfe866ded03c5
bigEyeChink <li.8906@buckeyemail.osu.edu>
2018-11-25 15:33:55 -0500

    update print all in activity`

--- app/src/main/java/com/demo/colin/demo/PreScheduleActivity.java
+++ app/src/main/java/com/demo/colin/demo/PreScheduleActivity.java
@@ -4,13 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 import java.util.HashSet;
 
 
 public class PreScheduleActivity extends Activity {
-    private ArrayList<String> availableClass = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
